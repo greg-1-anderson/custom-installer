d92992db5cd5dd323a943f02941a0a6f3727cadb
Greg Anderson <greg.1.anderson@greenknowe.org>
2014-12-09 11:00:37 -0800

    Override installCode, updateCode and removeCode to support installing packages with excluded directories.

--- CustomInstaller.php
+++ CustomInstaller.php
@@ -90,4 +90,151 @@ class CustomInstaller extends LibraryInstaller
       return $path;
   }
 
+  public function getExclusions($packageType)
+  {
+      if ($this->composer->getPackage()) {
+
+        $extra = $this->composer->getPackage()->getExtra();
+
+        if (!empty($extra['merge-exclusions'])) {
+
+          if (!empty($extra['merge-exclusions'][$packageType])) {
+            return $extra['merge-exclusions'][$packageType];
+          }
+
+        }
+
+      }
+
+      return false;
+  }
+
+  /**
+   * See:
+   *
+   * https://github.com/composer/composer/blob/master/src/Composer/Installer/LibraryInstaller.php
+   */
+  protected function installCode(PackageInterface $package)
+  {
+      $exclusions = getExclusions($package->getType());
+      if (empty($exclusions)) {
+          parent::installCode($package);
+      }
+      else {
+          $this->installCodeExceptExclusions($package, $exclusions);
+      }
+  }
+  protected function updateCode(PackageInterface $initial, PackageInterface $target)
+  {
+      $exclusions = getExclusions($package->getType());
+      if (empty($exclusions)) {
+          parent::updateCode($initial, $target);
+      }
+      else {
+          $this->updateCodeExceptExclusions($initial, $target, $exclusions);
+      }
+  }
+  protected function removeCode(PackageInterface $package)
+  {
+      $exclusions = getExclusions($package->getType());
+      if (empty($exclusions)) {
+          parent::updateCode($package);
+      }
+      else {
+          $this->removeCodePreservingExclusions($package, $exclusions);
+      }
+  }
+  // TODO: make protected once we are testing via the public interface
+  public function removePreservingExclusions($targetPath, $exclusions, $prefix = '')
+  {
+      // Scan through $targetPath.  Delete anything that is not
+      // in $exclusions.
+      foreach (new \DirectoryIterator($targetPath) as $fileInfo)
+      {
+          $fs_item = $fileInfo->getFilename();
+          $search_for = $prefix . $fs_item;
+          if (!$this->array_item_begins_with($search_for, $exclusions))
+          {
+              if ($fileInfo->isFile() || !$fileInfo->isDot())
+              {
+                  $this->filesystem->remove($targetPath . DIRECTORY_SEPARATOR . $fs_item);
+              }
+          }
+          if (!in_array($search_for, $exclusions))
+          {
+              if ($fileInfo->isDir() && !$fileInfo->isDot())
+              {
+                  $this->removePreservingExclusions($targetPath . DIRECTORY_SEPARATOR . $fs_item, $exclusions, $prefix . $fs_item . "/");
+              }
+          }
+      }
+  }
+  // TODO: make protected once we are testing via the public interface
+  public function mergeDirectoriesSkippingExclusions($sourcePath, $targetPath, $exclusions, $prefix = '')
+  {
+      $this->filesystem->ensureDirectoryExists($targetPath);
+      // Copy contents of $sourcePath to $targetPath.
+      // Anything in exclusions should remain in $targetPath.
+      // Anything not in $sourcePath or exclusions should be deleted.
+      foreach (new \DirectoryIterator($sourcePath) as $fileInfo)
+      {
+          $fs_item = $fileInfo->getFilename();
+          $search_for = $prefix . $fs_item;
+          if (!in_array($search_for, $exclusions))
+          {
+              if($fileInfo->isFile())
+              {
+                  copy($sourcePath . DIRECTORY_SEPARATOR . $fs_item, $targetPath . DIRECTORY_SEPARATOR . $fs_item);
+              }
+              elseif(!$fileInfo->isDot())
+              {
+                  $this->mergeDirectoriesSkippingExclusions($sourcePath . DIRECTORY_SEPARATOR . $fs_item, $targetPath . DIRECTORY_SEPARATOR . $fs_item, $exclusions, $prefix . $fs_item . "/");
+              }
+          }
+      }
+  }
+  private function array_item_begins_with($needle, $haystack) {
+      foreach ($haystack as $comparitor)
+      {
+          if (($comparitor == $needle) || (substr($comparitor, 0, strlen($needle) + 1) == $needle . "/"))
+          {
+              return true;
+          }
+      }
+      return false;
+  }
+  protected function installCodeExceptExclusions(PackageInterface $package, $exclusions)
+  {
+      $downloadPath = $this->getInstallPath($package);
+      // Create a temporary name next to the download location.
+      // We will remove this when we are done.  '$downloadPath'
+      // should already be unique at this point in time in the system,
+      // so we do not have to work too hard to get a decent tmpname.
+      $tmpPath = $downloadPath . '-' . (time() % 100000);
+      // Download to the temporary location using the existing download manager.
+      $this->downloadManager->download($package, $tmpPath);
+      // Delete everything in the target, except for the 'exclusions';
+      // then copy everything from the temporary download location to
+      // the final target, again skipping the 'exclusions'.
+      $this->removePreservingExclusions($downloadPath, $exclusions);
+      $this->mergeDirectoriesSkippingExclusions($tmpPath, $downloadPath, $exclusions);
+      $this->filesystem->removeDirectory($tmpPath);
+  }
+  protected function updateCodeExceptExclusions(PackageInterface $initial, PackageInterface $target, $exclusions)
+  {
+      // No optimizations here -- just remove the old code, and re-install the new.
+      $this->removeCodePreservingExclusions($initial, $exclusions);
+      $this->installCodeExceptExclusions($target, $exclusions);
+  }
+  protected function removeCodePreservingExclusions(PackageInterface $package, $exclusions)
+  {
+      $downloadPath = $this->getPackageBasePath($package);
+      $this->io->write("  - Removing <info>" . $package->getName() . "</info> (<comment>" . $package->getPrettyVersion() . "</comment>)");
+      // TODO: cleanChanges() is most likely going to be confused
+      // by the excluded directories, and will probably flag them
+      // as being changed.  We'll need our own version of this routine.
+      $this->cleanChanges($package, $downloadPath, false);
+      // Remove everything from $downloadPath except $exclusions
+      $this->removePreservingExclusions($downloadPath, $exclusions);
+  }
 }
--- Tests/CustomInstallerTest.php
+++ Tests/CustomInstallerTest.php
@@ -12,10 +12,169 @@ use Composer\Config;
 use Composer\Composer;
 use Composer\Package\Package;
 use Composer\Package\RootPackage;
+use Composer\Util\Filesystem;
 use PHPUnit_Framework_TestCase;
 
 class CustomInstallerTest extends PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @dataProvider dataForMergeDirectories
+     */
+    public function testMergeDirectories($source, $target, $exclusions, $expected)
+    {
+        $composer = new Composer();
+        $config = new Config();
+        $composer->setConfig($config);
+
+        $repository = $this->getMock('Composer\Repository\InstalledRepositoryInterface');
+        $io = $this->getMock('Composer\IO\IOInterface');
+        $installer = new CustomInstaller($io, $composer);
+
+        $sourceTree = '/tmp/source';
+        $targetTree = '/tmp/target';
+
+        $this->buildDirectoryTree($source, $sourceTree);
+        $this->buildDirectoryTree($target, $targetTree);
+
+        $installer->removePreservingExclusions($targetTree, $exclusions);
+        $installer->mergeDirectoriesSkippingExclusions($sourceTree, $targetTree, $exclusions);
+
+        $this->assertTreeMatchesExpected($expected, $targetTree);
+
+        // TODO: We should test by creating a package and calling 'install' and other public methods on it.
+        // Ideally, reference a local git repository stored in the 'test' folder.
+    }
+
+    public function dataForMergeDirectories()
+    {
+        return array(
+            array(
+                // source tree
+                array(
+                    "index.php" => "copy",
+                    ".htaccess" => "copy",
+                    "core" => array(
+                        "vendor" => array(
+                            "autoload.php" => "ignore",
+                        ),
+                        "install.php" => "copy",
+                    ),
+                    "modules" => array(
+                        "READEME" => "ignore",
+                    ),
+                ),
+                // target tree
+                array(
+                    "index.php" => "overwrite",
+                    ".htaccess" => "overwrite",
+                    "core" => array(
+                        "vendor" => array(
+                            "autoload.php" => "preserve",
+                        ),
+                        "install.php" => "overwrite",
+                        "kittens.php" => "remove",
+                    ),
+                    "modules" => array(
+                        "READEME" => "preserve",
+                    ),
+                ),
+                // exclusions
+                array(
+                    "core/vendor",
+                    "modules",
+                ),
+                // expected result
+                array(
+                    "index.php" => "copy",
+                    ".htaccess" => "copy",
+                    "core" => array(
+                        "vendor" => array(
+                            "autoload.php" => "preserve",
+                        ),
+                        "install.php" => "copy",
+                    ),
+                    "modules" => array(
+                        "READEME" => "preserve",
+                    ),
+                ),
+            ),
+        );
+    }
+
+    private function buildDirectoryTree($tree, $path)
+    {
+        $filesystem = new Filesystem();
+        $filesystem->remove($path);
+        $filesystem->ensureDirectoryExists($path);
+        foreach ($tree as $item => $data)
+        {
+            // The key is the filesystem item (file or
+            // directory) to create.  The data is a string
+            // to create a file, or a nested array to create
+            // a directory, perhaps with nested content.
+            $fs_item = $path . DIRECTORY_SEPARATOR . $item;
+            if (is_string($data))
+            {
+                file_put_contents($fs_item, $data);
+            }
+            else
+            {
+                $this->buildDirectoryTree($data, $fs_item);
+            }
+        }
+    }
+
+    private function assertTreeMatchesExpected($expected, $targetTree)
+    {
+        // First, insure that everything in $expected exists
+        // in the expected directory tree.
+        $this->assertPathContainsExpected($expected, $targetTree);
+        // Next, check to see if there is anything in the target
+        // tree that should not be there.
+        $this->assertPathDoesNotContainExtra($expected, $targetTree);
+    }
+
+    private function assertPathContainsExpected($tree, $path)
+    {
+        foreach ($tree as $item => $data)
+        {
+            // The key is the filesystem item (file or
+            // directory) to create.  The data is a string
+            // to create a file, or a nested array to create
+            // a directory, perhaps with nested content.
+            $fs_item = $path . DIRECTORY_SEPARATOR . $item;
+            if (is_string($data))
+            {
+                $contents = file_exists($fs_item) ? trim(file_get_contents($fs_item)) : "";
+                $expected = $item . ": " . $data;
+                $actual   = $item . ": " . $contents;
+                $this->assertEquals($expected, $actual);
+            }
+            else
+            {
+                $this->assertPathContainsExpected($data, $fs_item);
+            }
+        }
+    }
+    private function assertPathDoesNotContainExtra($tree, $path)
+    {
+        foreach (new \DirectoryIterator($path) as $fileInfo)
+        {
+            if (!$fileInfo->isDot())
+            {
+                $fs_item = $fileInfo->getFilename();
+                if (!array_key_exists($fs_item, $tree))
+                {
+                    $this->assertEquals("", $fs_item);
+                }
+                elseif ($fileInfo->isDir())
+                {
+                    $this->assertPathDoesNotContainExtra($tree[$fs_item], $path . DIRECTORY_SEPARATOR . $fs_item);
+                }
+            }
+        }
+    }
 
   /**
     * testInstallPath
